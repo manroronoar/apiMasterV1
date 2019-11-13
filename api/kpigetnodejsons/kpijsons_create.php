9db9d075de0bd1a10c243378705583037f02ce75
manroronoar <manroronoars@gmail.com>
2019-11-13 18:27:04 +0700

    update 13/11/2019
    -//http://localhost/api/api/kpigetnodejsons/kpijsons_create.php

--- api/kpigetnodejsons/kpijsons_create.php
+++ api/kpigetnodejsons/kpijsons_create.php
@@ -1,19 +1,20 @@
 <?php 
   // Headers
-  //http://localhost/api/api/kpigetnodejsons/headerset_getnode.php
+  //http://localhost/api/api/kpigetnodejsons/kpijsons_create.php
   //{
-  //"Gn_id" :"",
-  //"Gn_node" :"",
-  //"Gn_astid" :"",
-  //"Gn_iobit" :"",
-  //"Gn_ts" :"",
-  //"Gn_dmystr" :"",
-  //"Gn_hmstr" :"",
-  //"Gn_sec" :"",
-  //"Gn_tsupd" :"",
-  //"created_at" :"",
-  //"updated_at" :"",
-  //}
+  //  "Gn_id" :"44",
+  //  "Gn_node" :"-",
+  //  "Gn_astid" :"-",
+  //  "Gn_iobit" :"11223344",
+  //  "Gn_ts" :"157216",
+  //  "Gn_dmystr" :"2019-11-05",
+  //  "Gn_hmstr" :"23:23:36",
+  //  "Gn_sec" :"2",
+  //  "Gn_tsupd" :"1572996218",
+  //  "created_at" :"2019-11-07 00:45:23",
+  //  "updated_at" :"2019-11-07 00:45:23"
+  //  }
+
   header('Access-Control-Allow-Origin: *');
   header('Content-Type: application/json');
   header('Access-Control-Allow-Methods: POST');
@@ -29,38 +30,57 @@
   // Instantiate blog post object
   $getjson = new Getjsons($db);
   
-  
-
   // Get raw users data
-  $data = json_decode(file_get_contents("php://input"));
-  //$json = file_get_contents('php://input');
-  //$obj = json_decode($json,true);
-  //print_r(json_encode($users_arr, JSON_UNESCAPED_UNICODE))."\n";
-  //SELECT id, Gn_id, Gn_node, Gn_astid, Gn_iobit, Gn_ts, 
-  //Gn_dmystr, Gn_hmstr, Gn_sec, Gn_tsupd, created_at, 
-  //updated_at FROM kpi_getnodejsons WHERE 
-  
-  $getjson->Gn_id = $data->Gn_id;
-  $getjson->Gn_node = $data->Gn_node;
-  $getjson->Gn_astid = $data->Gn_astid;
-  $getjson->Gn_iobit = $data->Gn_iobit;
-  $getjson->Gn_ts = $data->Gn_ts;
-  $getjson->Gn_dmystr = $data->Gn_dmystr;
-  $getjson->Gn_hmstr = $data->Gn_hmstr;
-  $getjson->Gn_sec = $data->Gn_sec;
-  $getjson->Gn_tsupd = $data->Gn_tsupd;
-  $getjson->created_at = $data->created_at;
-  $getjson->updated_at = $data->updated_at;
+  //$data = json_decode(file_get_contents("php://input"));
+  $data = file_get_contents('php://input');
+  $obj_array = json_decode($data,true);
+
+    try 
+    {
+      foreach($obj_array as $row) 
+        {
+        $getjson->Gn_id = $row['Gn_id'];
+        $getjson->Gn_node = $row['Gn_node'];
+        $getjson->Gn_astid = $row['Gn_astid'];
+        $getjson->Gn_iobit = $row['Gn_iobit'];
+        $getjson->Gn_ts = $row['Gn_ts'];
+        $getjson->Gn_dmystr = $row['Gn_dmystr'];
+        $getjson->Gn_hmstr = $row['Gn_hmstr'];
+        $getjson->Gn_sec = $row['Gn_sec'];
+        $getjson->Gn_tsupd = $row['Gn_tsupd'];
+        $getjson->created_at = $row['created_at'];
+        $getjson->updated_at = $row['updated_at'];
+     
+        $getjson->create();
+        }
+    }   
+    catch( Exception $e )
+    { 
+      echo $e;
+    }
+
+  //$getjson->Gn_id = $data->Gn_id;
+  //$getjson->Gn_node = $data->Gn_node;
+  //$getjson->Gn_astid = $data->Gn_astid;
+  //$getjson->Gn_iobit = $data->Gn_iobit;
+  //$getjson->Gn_ts = $data->Gn_ts;
+  //$getjson->Gn_dmystr = $data->Gn_dmystr;
+  //$getjson->Gn_hmstr = $data->Gn_hmstr;
+  //$getjson->Gn_sec = $data->Gn_sec;
+  //$getjson->Gn_tsupd = $data->Gn_tsupd;
+  //$getjson->created_at = $data->created_at;
+  //$getjson->updated_at = $data->updated_at;
 
 
-  if($getjson->create()) {
-    echo json_encode(
-      array('message' => 'Created')
-    );
-  } else {
-    echo json_encode(
-      array('message' => 'Not Created')
-    );
-  }
+
+  //if($getjson->create()) {
+  //  echo json_encode(
+   //   array('message' => 'Created')
+   // );
+  //} else {
+  //  echo json_encode(
+  //    array('message' => 'Not Created')
+  //  );
+  //}
   
 
--- models/Getnodejsons.php
+++ models/Getnodejsons.php
@@ -50,22 +50,23 @@
    }
 
    private function check_bit($bit) {
-    //return str_split($bit,1);
-   // return strlen($bit);
-    //return str_split($bit);
-   // $rest = substr("abcdef", -3, 1);
+
         $countlen = strlen($bit);
-       // $rest = substr($bit, 0, 1);
-
-
-        foreach($test as $value) {
-       //$sum += $value;
-       }
-   
+        if($countlen == 8)
+       // echo $countlen;
+        {
+          for ($x = 0; $x < $countlen; $x++)
+          {
+            if(is_numeric($bit[$x]) == false)
+            {   
+               // echo $bit[$x];
+                return false;
+                break;
+            }      
+          } 
+          return true;  
+        }
    }
-
-
-
       
     public function create() {
     
@@ -79,20 +80,7 @@
    
       // Prepare statement
       $stmt = $this->conn->prepare($query);
-      // Clean data
-      //$filtered = array_map('htmlspecialchars', array_map('stripslashes', $row));
-      //$this->Gn_id = htmlspecialchars(strip_tags($this->Gn_id));
-     // $this->Gn_node = htmlspecialchars(strip_tags($this->Gn_node));
-     // $this->Gn_astid = htmlspecialchars(strip_tags($this->Gn_astid));
-     // $this->Gn_iobit = htmlspecialchars(strip_tags($this->Gn_iobit));
-     // $this->Gn_ts = htmlspecialchars(strip_tags($this->Gn_ts));
-     // $this->Gn_dmystr = htmlspecialchars(strip_tags($this->Gn_dmystr));
-     // $this->Gn_hmstr = htmlspecialchars(strip_tags($this->Gn_hmstr));
-     // $this->Gn_sec = htmlspecialchars(strip_tags($this->Gn_sec));
-    //  $this->Gn_tsupd = htmlspecialchars(strip_tags($this->Gn_tsupd));
-    //  $this->created_at = htmlspecialchars(strip_tags($this->created_at));
-    //  $this->updated_at = htmlspecialchars(strip_tags($this->updated_at));
-     
+   
       // Bind data
       $stmt->bindParam(':Gn_id', $this->Gn_id);
       $stmt->bindParam(':Gn_node', $this->Gn_node);
@@ -106,42 +94,22 @@
       $stmt->bindParam(':created_at', $this->created_at);
       $stmt->bindParam(':updated_at', $this->updated_at);
       
-       
-      // $str1 = $this->check_bit($this->Gn_iobit);
-       //print_r(array_count_values($str1));
-      // echo $str1;
-       //echo count($str1);
-       //foreach($test as $value) {
-       //$sum += $value;
-       //}
 
-       echo $this->Gn_iobit;
-
-       echo $this->Gn_iobit[0];
-       echo $this->Gn_iobit[1];
-       echo $this->Gn_iobit[2];
-
-       
-       
-      
       if ($num < 1 )
       {
-         // Execute query
+        if($this->check_bit($this->Gn_iobit) == true)
+        {
           if($stmt->execute()) {
-          //printf("Error: %s.\n", $stmt->error, $query);
           return true;
           }
           else
           {
-          // Print error if something goes wrong
-          // printf("Error: %s.\n", $stmt->error, $query);
           return false;
-          }      
+          }   
+        }   
       }
       else
       {
-      // Print error if something goes wrong
-      // printf("Error: %s.\n", $stmt->error, $query);
         return false;
       }     
     }
